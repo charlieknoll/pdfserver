12c69c9e36b827c90b50b63a88d8b6afeb2fbc44
Charlie Knoll <charlie.knoll@gmail.com>
2018-07-27 19:04:32 -0600

    User auth complete

--- app/controllers/users/changePassword.js
+++ app/controllers/users/changePassword.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt')
 
 const passport = require('passport')
 const url = '/user/change-password'
+const viewPath = url.substring(1)
 
 const actionVm = function (req, errors, email, displayname) {
     var errMsgs = [].map(e => e.msg)
@@ -12,23 +13,23 @@ const actionVm = function (req, errors, email, displayname) {
         title: 'Change password',
         errors: (errors || []).map(e => e.msg),
         email: email,
-        displayname: displayname
+        displayname: displayname,
+        resetToken: req.query.token
     }
 }
 
 const get = async function (req, res, next) {
-    //TODO validate reset-token, retrieve user name and email
+
     try {
         const resetToken = req.query.token
         const result = await db.any('Select email, displayname, tokenexpire < CURRENT_TIMESTAMP expired from users where resettoken = ${resetToken}', { resetToken })
 
-        //If invalid or expired reset-token then redirect to Reset-password with error message
         if (result.length === 0 || result[0].expired) {
             req.session.errorMessage = 'The reset token is either invalid or expired, please try again.'
             res.redirect('/user/reset-password')
             return
         }
-        res.render(url.substring(1), actionVm(req, null, result[0].email, result[0].displayname))
+        res.render(viewPath, actionVm(req, null, result[0].email, result[0].displayname))
     }
     catch (err) {
         logger.error(err)
@@ -42,47 +43,59 @@ const validate = [
     //validate form
     body('password', 'Password must be at least 8 characters').isLength({ min: 8 }),
     body('password').custom(async (password, { req }) => {
-        if (!/^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{8,})/.test(password))
+        if (!/^((?=.*[a-z])(?=.*[0-9])(?=.*[A-Z]))(?=.{8,})/.test(password))
             throw new Error("Password must contain at least 1 upper, lowercase and numeric character")
     }),
-    //TODO validate reset token not expired
 ]
 
 //handle validation errors
 const handleValidationErrors = function (req, res, next) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        const vm = actionVm(req, errors.array())
+        const vm = actionVm(req, errors.array(), req.body.username, req.body.displayname)
         res.render(viewPath, vm)
         return
     }
     next()
 }
-const post = function (req, res, next) {
-    //TODO add succcess message to dashboard
+const post = async function (req, res, next) {
 
-    //TODO update password in database
+    //TODO check that UPDATE worked
 
-    passport.authenticate('local', function (err, user, info) {
-        if (err) { return next(err); }
-        if (!user) {
-            const errs = [{ msg: "Invalid email or password" }]
-            res.render(url.substring(1), actionVm(req, errs))
-            return
-
+    try {
+        sqlParams = {
+            passwordHash: await bcrypt.hash(req.body.password, 10),
+            resetToken: req.query.token,
+            email: req.body.username
         }
-        req.logIn(user, function (err) {
+        const result = await db.result("UPDATE users SET passwordhash = ${passwordHash}, resettoken = null, tokenexpire = null where resettoken = ${resetToken} and email = ${email} and tokenexpire > CURRENT_TIMESTAMP", sqlParams)
+        if (result.rowCount !== 1) throw new Error("The reset token is either invalid or expired, please try again")
+
+        passport.authenticate('local', function (err, user, info) {
             if (err) { return next(err); }
-            var redirectTo = req.session.redirectTo ? req.session.redirectTo : '/user/dashboard';
-            delete req.session.redirectTo;
-            res.redirect(redirectTo);
-        });
+            if (!user) {
+                const errs = [{ msg: "Invalid email or password" }]
+                res.render(url.substring(1), actionVm(req, errs))
+                return
 
-    })(req, res, next)
+            }
+            req.logIn(user, function (err) {
+                if (err) { return next(err); }
+                var redirectTo = req.session.redirectTo ? req.session.redirectTo : '/user/dashboard';
+                delete req.session.redirectTo;
+                if (redirectTo === '/user/dashboard') req.session.successMessage = "Password successfully changed"
+                res.redirect(redirectTo);
+            });
 
-
+        })(req, res, next)
+    }
+    catch (err) {
+        logger.error(err)
+        req.session.errorMessage = 'Unknown error: the reset token is either invalid or expired, please try again.'
+        res.redirect('/user/reset-password')
+    }
 }
 module.exports = function (app) {
     app.get(url, get)
-    app.post(url, post)
+    app.post(url, validate, handleValidationErrors, post)
 }
--- app/controllers/users/dashboard.js
+++ app/controllers/users/dashboard.js
@@ -2,7 +2,9 @@ const url = '/user/dashboard'
 const auth = require('../../middlewares/authorization')
 
 const get = function (req, res, next) {
-    res.render(url.substring(1), { name: req.user.displayname, title: "Dashboard" })
+    const successMessage = req.session.successMessage
+    delete req.session.successMessage
+    res.render(url.substring(1), { name: req.user.displayname, title: "Dashboard", successMessage })
 }
 
 module.exports = function (app) {
