189aa9b30f7f67641ae62148f232d0e46d5deeb0
Charlie Knoll <charlie.knoll@gmail.com>
2019-09-11 16:09:06 -0600

    env debugging

--- app/controllers/users/dashboard.js
+++ app/controllers/users/dashboard.js
@@ -2,13 +2,19 @@ const url = '/user/dashboard'
 const auth = require('../../middlewares/authorization')
 const { db, logger } = require('../../services')
 const asyncHandler = require('express-async-handler')
+const config = require('../../config')
 
 
 const get = async function (req, res, next) {
     const successMessage = req.session.successMessage
     delete req.session.successMessage
     const result = await db.any('Select value, usedcredits from apikey where userid = ${userid}', { userid: req.user.id })
-    res.render(url.substring(1), { name: req.user.displayname, apikey: result[0].value, usedCredits: result[0].usedcredits, title: "Dashboard", successMessage })
+    res.render(url.substring(1), {
+        name: req.user.displayname,
+        apikey: result[0].value, usedCredits: result[0].usedcredits,
+        title: "Dashboard", successMessage,
+        env: JSON.stringify(config)
+    })
 }
 
 module.exports = function (app) {
