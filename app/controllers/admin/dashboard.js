a164300eb832ef678d21fe6bf645a0975514e72a
Charlie Knoll <charlie.knoll@gmail.com>
2019-10-24 16:45:04 -0600

    Fix for removing slice-file

--- app/controllers/admin/dashboard.js
+++ app/controllers/admin/dashboard.js
@@ -1,26 +1,13 @@
 const asyncHandler = require('express-async-handler')
-//const { promisify } = require('util')
 const config = require('../../config')
-var sf = require('slice-file');
-//const sliceReverseAsync = promisify(sf.sliceReverse)
-const { streamToString } = require('../../util')
 const logger = require('../../services/logger')
 
 
 module.exports = function (router) {
 
   const get = async function (req, res, next) {
-    //throw new Error("test")
     const xs = sf(config.logFile)
     let logs
-    // try {
-    //   logs = await streamToString(xs.sliceReverse(-100))
-    //   //this throws invalid JSON error
-    //   //const logs = JSON.parse(logStr.trim())
-    // } finally {
-    //   xs.close()
-
-    // }
     var options = {
       from: new Date - 24 * 60 * 60 * 1000,
       until: new Date,
