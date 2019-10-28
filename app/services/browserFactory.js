61b01524cf89181c08dc944d4d3961584002be23
Charlie Knoll <charlie.knoll@gmail.com>
2019-10-27 18:01:51 -0600

    Adds window size to start up

--- app/services/browserFactory.js
+++ app/services/browserFactory.js
@@ -1,7 +1,7 @@
 var debug = require('debug')('pdfserver:browser');
 const puppeteer = require('puppeteer');
 const launchOptions = {}
-launchOptions.args = ["--incognito", "--no-sandbox", "--disable-gpu"];
+launchOptions.args = ["--incognito", "--no-sandbox", "--disable-gpu", "--window-size=1920,5000"];
 launchOptions.ignoreHTTPSErrors = true;
 launchOptions.pipe = true;
 module.exports = puppeteer.launch(launchOptions)
