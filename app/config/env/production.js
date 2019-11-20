bfa29d1fff09382b0ed5a41ac81e5f8dbe069355
Charlie Knoll <charlie.knoll@gmail.com>
2019-11-19 18:35:20 -0700

    Production env

--- app/config/env/production.js
+++ app/config/env/production.js
@@ -28,7 +28,7 @@ module.exports = {
 
 	},
 	braintree: {
-		environment: braintree.Environment.Sandbox,
+		environment: braintree.Environment.Production,
 		merchantId: process.env.BRAINTREE_MERCHANT_ID,
 		publicKey: process.env.BRAINTREE_PUBLIC_KEY,
 		privateKey: process.env.BRAINTREE_PRIVATE_KEY,
